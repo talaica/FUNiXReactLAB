734e154509f242c25c158a93a2e21b22918a5589
Do Thuy <talaica1001@gmail.com>
2022-03-21 16:30:28 +0900

    Exercise 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Contact from'./ContactComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishdetailComponent';
@@ -36,6 +37,12 @@ class Main extends Component {
       );
     }
 
+    const AboutUs = () => {
+      return(
+        <About leaders={this.state.leaders} />
+      );
+    }
+
     const DishWithId = ({match}) => {
       return(
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
@@ -51,6 +58,7 @@ class Main extends Component {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={AboutUs} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
